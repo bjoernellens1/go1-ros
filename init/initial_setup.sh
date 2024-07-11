4d8f1c8572c65b444fa978ea29a59c0b2813126e
Your Name <you@example.com>
2024-07-11 09:19:49 +0200

    update

--- init/initial_setup.sh
+++ init/initial_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup script for mandatory initial setup tasks
-sudo cp bin/pull_git_repo.sh /usr/local/bin/pull_git_repo.sh
+sudo cp pull_git_repo.sh /usr/local/bin/pull_git_repo.sh
 sudo chmod +x /usr/local/bin/pull_git_repo.sh
 
 sudo cp pull-git-repo.service /etc/systemd/system/pull-git-repo.service
