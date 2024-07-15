fa550b9c4d3883c9af446db3fdac013ebfdcbaeb
Your Name <you@example.com>
2024-07-15 10:44:09 +0200

    update

--- init/pull_git_repo.sh
+++ init/pull_git_repo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define the directory where the repository will be cloned
-REPO_DIR="/home/go1user/git/go1-ros-melodic"
+REPO_DIR="/home/go1user/git/go1-ros"
 
 # If the directory doesn't exist, clone the repository
 if [ ! -d "$REPO_DIR" ]; then
-  git clone https://github.com/bjoernellens1/go1-ros-melodic "$REPO_DIR"
+  git clone https://github.com/bjoernellens1/go1-ros "$REPO_DIR"
 else
   cd "$REPO_DIR"
   git pull origin main
